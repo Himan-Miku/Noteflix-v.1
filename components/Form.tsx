fe6e8c752cd6336154795f6a3753f0b51718f5e8
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-03 02:18:44 +0530

    Added form and backgrounds

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,7 +1,109 @@
-import React from "react";
+"use client";
+
+import { BsFillPatchPlusFill } from "react-icons/bs";
+import { useState, useRef, useEffect, RefObject, FormEvent } from "react";
+import { useRouter } from "next/navigation";
 
 const Form = () => {
-  return <div>Form</div>;
+  const router = useRouter();
+  const formRef = useRef(null);
+
+  const [inputs, setInputs] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [showInput, setShowInput] = useState(false);
+
+  function useOutsideAlerter(ref: RefObject<HTMLFormElement>) {
+    useEffect(() => {
+      function handleClickOutside(event: any) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowInput(false);
+        }
+      }
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+  useOutsideAlerter(formRef);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const noteBody = await fetch("/api/createNote", {
+      method: "POST",
+      body: JSON.stringify({
+        title: inputs.title,
+        content: inputs.content,
+      }),
+    });
+    const res = await noteBody.json();
+    console.log("🚀 ~ file: Form.tsx:28 ~ submitData ~ res:", res);
+
+    setInputs({
+      title: "",
+      content: "",
+    });
+
+    router.refresh();
+  };
+
+  const handleShowInput = () => {
+    setShowInput(true);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <form
+        className="w-1/2 rounded-lg outline outline-1 outline-gray-400 p-3 shadow-black shadow-lg"
+        onSubmit={handleSubmit}
+        autoComplete="off"
+        ref={formRef}
+      >
+        {showInput && (
+          <input
+            className="bg-[#202124] px-4 py-2 focus:outline-none w-full text-white font-semibold placeholder:font-semibold caret-white"
+            placeholder="Title"
+            value={inputs.title}
+            onChange={(e) => setInputs({ ...inputs, title: e.target.value })}
+            required
+          />
+        )}
+
+        <textarea
+          onClick={handleShowInput}
+          onInput={(e) => {
+            e.currentTarget.style.setProperty("height", "1px");
+            e.currentTarget.style.setProperty(
+              "height",
+              e.currentTarget.scrollHeight + "px"
+            );
+          }}
+          className={`bg-[#202124] px-4 py-2 focus:outline-none w-full placeholder:font-semibold placeholder:text-sm caret-white resize-none overflow-hidden max-h-40 text-white`}
+          placeholder="Take a note..."
+          value={inputs.content}
+          onChange={(e) => setInputs({ ...inputs, content: e.target.value })}
+          autoFocus
+          rows={1}
+          required
+        />
+
+        {showInput && (
+          <div className="flex justify-end items-center">
+            <button className="rounded-full" type="submit">
+              <BsFillPatchPlusFill
+                style={{ fontSize: "2rem", color: "#9f404085" }}
+              />
+            </button>
+          </div>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default Form;
---
+++ utils/backgrounds.ts
@@ -0,0 +1,114 @@
+export const colors = [
+  {
+    id: "default",
+    colour: "transparent",
+    imgSvgSrc: "/default.png",
+  },
+  {
+    id: "red",
+    colour: "#5C2B29",
+  },
+  {
+    id: "orange",
+    colour: "#614A19",
+  },
+  {
+    id: "yellow",
+    colour: "#635D19",
+  },
+  {
+    id: "green",
+    colour: "#345920",
+  },
+  {
+    id: "teal",
+    colour: "#16504b",
+  },
+  {
+    id: "blue",
+    colour: "#2d555e",
+  },
+  {
+    id: "darkblue",
+    colour: "#1e3a5f",
+  },
+  {
+    id: "purple",
+    colour: "#42275e",
+  },
+  {
+    id: "pink",
+    colour: "#5b2245",
+  },
+  {
+    id: "brown",
+    colour: "#442f19",
+  },
+  {
+    id: "gray",
+    colour: "#3c3f43",
+  },
+];
+
+export const bgImages = [
+  {
+    imageId: 1,
+    svgSrc: "",
+    coverPic: "/not_supported_img.png",
+  },
+  {
+    imageId: 2,
+    svgSrc: "https://www.gstatic.com/keep/backgrounds/grocery_dark_0609.svg",
+    coverPic:
+      "https://www.gstatic.com/keep/backgrounds/grocery_dark_thumb_0615.svg",
+  },
+  {
+    imageId: 3,
+    svgSrc: "https://www.gstatic.com/keep/backgrounds/food_dark_0609.svg",
+    coverPic:
+      "https://www.gstatic.com/keep/backgrounds/food_dark_thumb_0615.svg",
+  },
+  {
+    imageId: 4,
+    svgSrc: "https://www.gstatic.com/keep/backgrounds/music_dark_0609.svg",
+    coverPic:
+      "https://www.gstatic.com/keep/backgrounds/music_dark_thumb_0615.svg",
+  },
+  {
+    imageId: 5,
+    svgSrc: "https://www.gstatic.com/keep/backgrounds/recipe_dark_0609.svg",
+    coverPic:
+      "https://www.gstatic.com/keep/backgrounds/recipe_dark_thumb_0615.svg",
+  },
+  {
+    imageId: 6,
+    svgSrc: "https://www.gstatic.com/keep/backgrounds/notes_dark_0714.svg",
+    coverPic:
+      "	https://www.gstatic.com/keep/backgrounds/notes_dark_thumb_0715.svg",
+  },
+  {
+    imageId: 7,
+    svgSrc: "https://www.gstatic.com/keep/backgrounds/places_dark_0609.svg",
+    coverPic:
+      "https://www.gstatic.com/keep/backgrounds/places_dark_thumb_0615.svg",
+  },
+  {
+    imageId: 8,
+    svgSrc: "	https://www.gstatic.com/keep/backgrounds/travel_dark_0609.svg",
+    coverPic:
+      "https://www.gstatic.com/keep/backgrounds/travel_dark_thumb_0615.svg",
+  },
+  {
+    imageId: 9,
+    svgSrc: "	https://www.gstatic.com/keep/backgrounds/video_dark_0609.svg",
+    coverPic:
+      "https://www.gstatic.com/keep/backgrounds/video_dark_thumb_0615.svg",
+  },
+  {
+    imageId: 10,
+    svgSrc:
+      "https://www.gstatic.com/keep/backgrounds/celebration_dark_0714.svg",
+    coverPic:
+      "https://www.gstatic.com/keep/backgrounds/celebration_dark_thumb_0715.svg",
+  },
+];
