31cf493e979665eea7491232568012368bdd62c3
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-03 00:04:45 +0530

    nextauth: google auth and prisma adapter

---
+++ app/api/hello/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import client from "../../../../prisma/prismaClient";
+
+export const authOptions = {
+  adapter: PrismaAdapter(client),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
