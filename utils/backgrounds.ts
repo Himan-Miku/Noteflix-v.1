6f62fbefd53bd9cfea812de695ec06b79ed018a5
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-09 20:00:45 +0530

    Added anime backgrounds

--- utils/backgrounds.ts
+++ utils/backgrounds.ts
@@ -54,61 +54,139 @@ export const bgImages = [
   {
     imageId: 1,
     svgSrc: "",
-    coverPic: "/not_supported_img.png",
+    coverPic: "/default.png",
   },
   {
     imageId: 2,
-    svgSrc: "https://www.gstatic.com/keep/backgrounds/grocery_dark_0609.svg",
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/bd55a76955a9780678567a608adaf3ae/raw/18a89dfba58cc4b9032e48ab2d9e1f6aedfa6094/miku.svg",
     coverPic:
-      "https://www.gstatic.com/keep/backgrounds/grocery_dark_thumb_0615.svg",
+      "https://gistcdn.githack.com/Himan-Miku/a9369d59c625e169f53dfff3e97b22e3/raw/00512a29ca4991d38ecd548d79018f05fe7daade/miku2.svg",
   },
   {
     imageId: 3,
-    svgSrc: "https://www.gstatic.com/keep/backgrounds/food_dark_0609.svg",
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/6da14cc64269d6ad46c6a95b313cbfa9/raw/823edf2d66c12f2b6cebd62a48451b348b519102/aot.svg",
     coverPic:
-      "https://www.gstatic.com/keep/backgrounds/food_dark_thumb_0615.svg",
+      "https://gistcdn.githack.com/Himan-Miku/b5f5b3b4e9268ce817d3b2a5f755d561/raw/8885d82afdea85b414c522507853f2e2242e1f06/aot2.svg",
   },
   {
     imageId: 4,
-    svgSrc: "https://www.gstatic.com/keep/backgrounds/music_dark_0609.svg",
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/c02d647ad223c0a6eb4c9b74332c7dea/raw/62efc9fd6783b5c0802095546368ed88851c503e/bungou.svg",
     coverPic:
-      "https://www.gstatic.com/keep/backgrounds/music_dark_thumb_0615.svg",
+      "https://gistcdn.githack.com/Himan-Miku/e659b2c79958132482a2a112151575d8/raw/465e860c24b3638704991f036ea18e1362967d82/bungou2.svg",
   },
   {
     imageId: 5,
-    svgSrc: "https://www.gstatic.com/keep/backgrounds/recipe_dark_0609.svg",
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/562cbc7702105cd44e9179af3b6fbcb8/raw/6e22469faced7febd0f5b00e33dc4f0285507a0b/hdxd.svg",
     coverPic:
-      "https://www.gstatic.com/keep/backgrounds/recipe_dark_thumb_0615.svg",
+      "https://gistcdn.githack.com/Himan-Miku/7ee396a20d1b43df673d4aacca55454b/raw/787f43d4fdb45a4889372ea86395fbbf1685886e/hdxd2.svg",
   },
   {
     imageId: 6,
-    svgSrc: "https://www.gstatic.com/keep/backgrounds/notes_dark_0714.svg",
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/0d1342ae31c9c233aa4e650d11098d90/raw/f3cc813eab184c3dd767e7d9e7177d8f0d788458/hxh.svg",
     coverPic:
-      "	https://www.gstatic.com/keep/backgrounds/notes_dark_thumb_0715.svg",
+      "https://gistcdn.githack.com/Himan-Miku/2ae4d3ae51df3a84d41d66f30d499c38/raw/52d5102d8d5cf72f18efc0248f2cbddd734cabfb/killua.svg",
   },
   {
     imageId: 7,
-    svgSrc: "https://www.gstatic.com/keep/backgrounds/places_dark_0609.svg",
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/25209571487228b46889486f0892ea6e/raw/8b58d485adbe6e766b0e54791a9095f3147ebb14/itachi.svg",
     coverPic:
-      "https://www.gstatic.com/keep/backgrounds/places_dark_thumb_0615.svg",
+      "https://gistcdn.githack.com/Himan-Miku/e924e8484609f77f8601dd3592b54654/raw/9f4aa500e536350896dd20019322a9221c774582/itachi2.svg",
   },
   {
     imageId: 8,
-    svgSrc: "	https://www.gstatic.com/keep/backgrounds/travel_dark_0609.svg",
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/9755977eba78de056a1e1285925f842d/raw/b53431cb2cc771cbe21bb56d60ab69c4868d2392/jinchuriki.svg",
     coverPic:
-      "https://www.gstatic.com/keep/backgrounds/travel_dark_thumb_0615.svg",
+      "https://gistcdn.githack.com/Himan-Miku/ba2973a83fb7e3e30cc3ba09b193b462/raw/bd82c6c39c49b7a5f022c8a24f4079bcb0ae66c4/jin2.svg",
   },
   {
     imageId: 9,
-    svgSrc: "	https://www.gstatic.com/keep/backgrounds/video_dark_0609.svg",
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/dca97ee80e94edc66caef62b4ff9aee9/raw/711983bffe5c1474b075e1718bd852ee631ccac1/juju.svg",
     coverPic:
-      "https://www.gstatic.com/keep/backgrounds/video_dark_thumb_0615.svg",
+      "https://gistcdn.githack.com/Himan-Miku/5c900c0e8f8e28726afd4da0bf978116/raw/647e4b27b53ecc8ffcd31d852bae75fe4711deab/juju2.svg",
   },
   {
     imageId: 10,
     svgSrc:
-      "https://www.gstatic.com/keep/backgrounds/celebration_dark_0714.svg",
+      "https://gistcdn.githack.com/Himan-Miku/b013e7819e13fc84eefcacf798fe555c/raw/09ed8a5cf5b551161838797825a2aecf1ae4a58a/kaneki.svg",
     coverPic:
-      "https://www.gstatic.com/keep/backgrounds/celebration_dark_thumb_0715.svg",
+      "https://gistcdn.githack.com/Himan-Miku/9bdb4e6a45b8d1e8d22d64d1401492d9/raw/59773830bd45c2ea14022e7eb67a2d85f7529e69/kaneki2.svg",
+  },
+  {
+    imageId: 11,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/0b28d540e5f227a13659881afe504a83/raw/4d9d3f367d13fc98983e68d2555f36ef5e525fca/kurumi.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/e23b050b2d7d99510e82a815740f1290/raw/9872e81ef2a8d29cbf0ad3be7ba8bacb5a71348e/kurumi2.svg",
+  },
+  {
+    imageId: 12,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/ad343020b66acbcc3b0bd7181ad79d9d/raw/69cde544004413ee8e1ac58e001f86f8f4b05ae4/megumin.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/ceeea1d0b7a88d074c3c16cb8fd9924d/raw/d1189f772650792166e3546dede2d2dbc98cc679/megumin2.svg",
+  },
+  {
+    imageId: 13,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/588977f50545d1e3ed57433ef3c53976/raw/7a3bf6d393047f17b7a9a3b37cbd810c254ada9e/mha.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/dc3c11cbbb8510860179250fb448a7ef/raw/ed46059cec6b4253b9a3273df57afca9db21520e/mha2.svg",
+  },
+  {
+    imageId: 14,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/73e2fba400d03d4ce6f0b89f9c74fe05/raw/53738d8905fb3ac9d3fbdc30ebbd10cf3222538d/mikasa.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/6d61f0e9cd71942e9266cec8f58d7caa/raw/bb86785870019d18c9359c07b2a2d01995ac13ca/mikasa2.svg",
+  },
+  {
+    imageId: 15,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/9b58011c0073e088b533762254b9a912/raw/c3392d06bd13637646d7789bf068074eb0c9b5bd/nezuTan.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/8f5a268a44bd36151f6ad1c2c1588bbf/raw/b69f62bac3bad953ec110d3d414932e2a8ce0d2e/demon.svg",
+  },
+  {
+    imageId: 16,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/28b45db53933981cd5d5591d9f085e48/raw/13c36a1bff7362845c017615b77947a6001a239c/nino.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/26dcc3882c71dd56ff9b82705c358586/raw/0ee84189896165179295fe3fd20e7169efa5702c/nino2.svg",
+  },
+  {
+    imageId: 17,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/0c033f4e51db11ab3d21b0e4ae86789b/raw/6ed73c66e22bdbc7f415ed372018d13a2e5b81ed/sasuke.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/6982daaaa13f31dd547d9c1566774113/raw/b6e819f1368351a014e24592f33ec1b905c71ad9/sasuke2.svg",
+  },
+  {
+    imageId: 18,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/d8c48c98fdb9d18a56a5c68fcbd3c5f0/raw/9ffef960901257ea7288bed7ac72d37dedc1b6f8/solo.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/bc8df2977fff22ae1a97f4301dd38a86/raw/14c7618a4a78d5b989faf34e687b8cacd0bfd405/solo2.svg",
+  },
+  {
+    imageId: 19,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/ad691062849592473ae82a6a63f2220e/raw/f5e44ad3f7b0fc92d217d56dcd58da5a127cd683/zoro.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/2f950ca23c15f09f159ee17907046042/raw/df518ea8563772b9482bf7e488dec7e0e336ce86/zoro2.svg",
+  },
+  {
+    imageId: 20,
+    svgSrc:
+      "https://gistcdn.githack.com/Himan-Miku/ea08c27fd34e2aa0f5c9e95dc168af51/raw/f1348ee0019ea2e6ffe6a9de23b68f7d0fad4871/team7.svg",
+    coverPic:
+      "https://gistcdn.githack.com/Himan-Miku/33113b2940c7842f32a72fe2d03a0626/raw/c1a1c49db4ae2a8336bbd0e6c5a2dba0d825f6b1/team777.svg",
   },
 ];
